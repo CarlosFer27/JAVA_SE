c95a8b18595ff6c18ce8c2379f1602ef4cc0cef4
CarlosFer27 <juancarloshernandezcono@hotmail.com>
2017-01-16 22:54:45 -0600

    Commit con ejercicios del video 4 del Curso Java SE

---
+++ Video_SE4/Hola_Mundo.java
@@ -0,0 +1,9 @@
+package Video_SE4;
+
+public class Hola_Mundo {
+	
+	public static void main(String[] args){
+		System.out.println("hola mi Fernandita");
+	}
+
+}
---
+++ Video_SE4/Metodo_Main.java
@@ -0,0 +1,10 @@
+package Video_SE4;
+
+public class Metodo_Main {
+	
+	public static void main(String[] args){
+		
+		System.out.println(args.length);
+	}
+
+}
