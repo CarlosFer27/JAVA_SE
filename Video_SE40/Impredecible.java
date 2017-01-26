04f51503ea21bbdce5b6a8c75cf169d24efa1514
CarlosFer27 <juancarloshernandezcono@hotmail.com>
2017-01-25 23:35:58 -0600

    Commit con ejercicios del Video 40 del curso Java SE

---
+++ Video_SE40/Impredecible.java
@@ -0,0 +1,21 @@
+package Video_SE40;
+import java.util.HashSet;
+
+public class Impredecible {
+	public static void main(String[] args){
+		
+		HashSet<String> lista = new HashSet<String>();
+		
+		lista.add("Fernanda");
+		lista.add("Jacqueline");
+		lista.add("Luz");
+		lista.add("Shrek");
+		lista.add("Muñe");
+		
+		for (String muestra : lista){
+			System.out.println(muestra);
+		}
+		
+	}
+
+}
