e1c11382327219f2fa09df94b2860a0fce6ac03e
CarlosFer27 <juancarloshernandezcono@hotmail.com>
2017-01-25 23:05:13 -0600

    Commit con ejercicio del Video 38 del curso Java SE

---
+++ Video_SE38/LinkedList_Example.java
@@ -0,0 +1,14 @@
+package Video_SE38;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedList_Example {
+
+	public static void main(String[] args){
+		
+		LinkedList linkedList = new LinkedList();
+		
+		linkedList.add("Hola");
+		
+	}
+}
